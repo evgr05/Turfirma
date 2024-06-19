e3254cd6fd2d07e4057bf9a6270c45f6b4524847
admin <admin@COMP4>
2024-06-19 20:37:54 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Pages/PageLogin.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.DataFilesApp;
+using WpfApp1.Pages;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// авторизация пользователей
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var userObj = OdbConnectHelper.entObj.User.FirstOrDefault(x => x.Login == txbLogin.Text && x.Password == psbPassword.Password);
+            if (userObj != null)
+            {
+                switch (userObj.Role)
+                {
+                    case 1:
+                        FrameApp.frmObj.Navigate(new PageAdmin());
+                        break;
+
+                        case 2:
+                        FrameApp.frmObj.Navigate(new PageUser());
+                        break;
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неправильный пароль или логин, попробуйте ещё раз", "Ошибка",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
+ 
